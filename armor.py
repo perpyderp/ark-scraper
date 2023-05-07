b5f4fd5cb652c6521e2f1e727f66f83c2f674863
perpyderp <g4m3rall3n@gmail.com>
2023-05-06 22:43:03 -0400

    Parsed ingredient field as array of ingredients

--- armor.py
+++ armor.py
@@ -13,21 +13,33 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
+# Regular expression package
+import re
+
+def parse_ingredients(string):
+    ingredients = []
+    pattern = r'(\d+)\s*[×x]\s*([A-Za-z]+)'
+    matches = re.findall(pattern, string)
+    for match in matches:
+        quantity = int(match[0])
+        material = match[1].strip()
+        ingredient = {'quantity': quantity, 'material': material}
+        ingredients.append(ingredient)
+    return ingredients
+
+
 # Loading environment files from .env
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-USER = os.getenv('DB_USER')
-PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
-CLUSTER = os.getenv('CLUSTER_NAME')
 
 # Connect to database based using environment variables
-client = MongoClient("mongodb+srv://" + USER + ":" + PASSWORD + "@" + CLUSTER + ".mongodb.net/" + DB_NAME + "?retryWrites=true&w=majority")
+client = MongoClient(os.getenv('MONGODB_URI'))
 
 db = client[DB_NAME]
 
-collection = db['armor']
+collection = db['armors']
 
 # Make GET request to ARK Fandom
 r = requests.get('https://ark.fandom.com/wiki/Armor')
@@ -53,19 +65,20 @@ for armorRow in armorTableRows:
         armorData = [ele.text.strip() for ele in armorData]
 
         newArmor = {
-            "armor_type": armorData[0],
-            "unlock_level": armorData[1], 
-            "armor_rating": armorData[2], 
-            "cold_protection": armorData[3],
-            "heat_protection": armorData[4],
+            "armorType": armorData[0],
+            "unlockLevel": armorData[1], 
+            "armorRating": armorData[2], 
+            "coldProtection": armorData[3],
+            "heatProtection": armorData[4],
             "weight": armorData[5],
             "durability": armorData[6],
-            "found_in": armorData[7],
-            "ingredients": armorData[8],
+            "foundIn": armorData[7],
+            "ingredients": parse_ingredients(armorData[8]),
             "url": armorLink
         }
 
         data.append(newArmor)
 
-
 collection.insert_many(data)
+
+client.close()
